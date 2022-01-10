680a53410511d40962a4bf9cc49870f5960e4a82
kiranjoshi <keronz7777@gmail.com>
2022-01-10 12:20:12 +0545

    1

---
+++ android/app/src/main/kotlin/keronz/example/flutter_concept_challenges/MainActivity.kt
@@ -0,0 +1,6 @@
+package keronz.example.flutter_concept_challenges
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: 500.0,
+        width: double.infinity,
+        color: Colors.amber,
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 200.0,
+          width: 200.0,
+          color: Colors.purple,
+        ),
+      ),
+    );
+  }
+}
